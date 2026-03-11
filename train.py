b4cde3aa5dde32b7d615c76bdb8a6201b7b3e75f
autoresearch <autoresearch@local>
2026-03-11 14:53:32 +0000

    exp: omni_high_text: t0.7+tts0.15+asr0.15

--- train.py
+++ train.py
@@ -613,7 +613,7 @@ optimizer = model.setup_optimizer(
 
 model = torch.compile(model, dynamic=False)
 
-train_loader = make_omni_dataloader(tokenizer, DEVICE_BATCH_SIZE, MAX_SEQ_LEN, "train", text_ratio=0.6, audio_ratio=0.0, tts_ratio=0.2, asr_ratio=0.2)
+train_loader = make_omni_dataloader(tokenizer, DEVICE_BATCH_SIZE, MAX_SEQ_LEN, "train", text_ratio=0.7, audio_ratio=0.0, tts_ratio=0.15, asr_ratio=0.15, cross_modal_weight=1.0)
 x, y, epoch = next(train_loader)  # prefetch first batch
 
 print(f"Time budget: {TIME_BUDGET}s")
@@ -655,7 +655,7 @@ while True:
         train_loss = loss.detach()
         loss = loss / grad_accum_steps
         loss.backward()
-        x, y, epoch = next(train_loader)
+        x, y, epoch, _rw = next(train_loader)
 
     # H2: Progressive audio upweighting
     if AUDIO_UPWEIGHT_SCHEDULE:
