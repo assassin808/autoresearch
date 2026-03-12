6747376298d6f475760b562844d81e3d41ca6fe7
autoresearch <autoresearch@local>
2026-03-12 11:01:27 +0000

    exp: abl10: -coupledWD

--- train.py
+++ train.py
@@ -635,7 +635,7 @@ def get_muon_momentum(step):
     return (1 - frac) * 0.85 + frac * 0.97
 
 def get_weight_decay(progress):
-    return WEIGHT_DECAY * get_lr_multiplier(progress)  # coupled WD (sweep16: decays with LR)
+    return WEIGHT_DECAY  # constant WD
 
 # ---------------------------------------------------------------------------
 # Training loop
